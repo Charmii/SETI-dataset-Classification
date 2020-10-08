3a49c834acdfae738e3767dcf74c380b156d06eb
Charmi Shah <charmishah1030@gmail.com>
2020-10-08 22:38:29 +0530

    Update model.py

--- model.py
+++ model.py
@@ -49,3 +49,49 @@ model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.Dropout(0.25))
 model.add(keras.layers.Dense(4,activation = 'softmax'))
 
+
+
+initial_lr = 0.005
+lrschedule = keras.optimizers.schedules.ExponentialDecay(
+initial_learning_rate = initial_lr,
+decay_steps = 5,
+decay_rate = 0.96,
+staircase = True,
+)
+optimizer = keras.optimizers.Adam(learning_rate = lrschedule)
+
+
+model.compile(optimizer = optimizer,
+             loss = 'categorical_crossentropy',
+             metrics = ['accuracy'])
+
+
+
+batch_size = 32
+history = model.fit_generator(datagen_train.flow(xtrain,ytrain,batch_size = batch_size,shuffle = True),
+                    steps_per_epoch = len(xtrain)//batch_size,
+                    validation_data = datagen_val.flow(xval,yval,batch_size=batch_size, shuffle = True),
+                    validation_steps = len(xval)//batch_size,
+                    epochs = 15,
+                   )
+
+
+model.evaluate(xval,yval)
+#25/25 [==============================] - 2s 85ms/step - loss: 0.5731 - accuracy: 0.6821
+#[0.5730670094490051, 0.682102620601654]
+
+y_true = np.argmax(yval,1)
+y_pred = np.argmax(model.predict(xval),1)
+print(sklearn.metrics.classification_report(y_true,y_pred))
+#              precision    recall  f1-score   support
+#           0       0.81      0.99      0.89       199
+#           1       0.50      0.28      0.36       200
+#           2       0.52      0.45      0.48       200
+#           3       0.75      1.00      0.85       200
+#    accuracy                           0.68       799
+#   macro avg       0.64      0.68      0.65       799
+#weighted avg       0.64      0.68      0.65       799
+
+print(sklearn.metrics.accuracy_score(y_true,y_pred)) #0.6821026282853567
+
+
